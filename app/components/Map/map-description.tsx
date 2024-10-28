44457a4c6ed065a9ae8caedb3d00c1a5b8e45047
syouzen <seungyeon.choi1026@gmail.com>
2024-10-28 09:14:20 +0900

    🌱 map desc styles

--- app/components/Map/map-description.tsx
+++ app/components/Map/map-description.tsx
@@ -4,7 +4,7 @@ const MapDescription = () => {
   return (
     <div className={styles.descWrapper}>
       <div className={styles.desc}>
-        <span className={styles.descTitle}>지하철</span>
+        <div className={styles.descTitle}>지하철</div>
         <div className={styles.descContent}>
           <span>
             <b>지하철 3호선</b> 안국역 1번출구 도보 10분 거리에 위치
@@ -12,17 +12,17 @@ const MapDescription = () => {
         </div>
       </div>
       <div className={styles.desc}>
-        <span className={styles.descTitle}>주차</span>
+        <div className={styles.descTitle}>주차</div>
         <div className={styles.descContent}>
           <span>건물 내 15대 무료</span>
-          <div className="flex gap-0.5">
+          <div className={styles.descContentSub}>
             *
             <span>
-              주차장 이용이 혼잡합니다. 불편하시더라도 대중교통 이용을 권장
-              드립니다.
+              주차장 이용이 혼잡할 수 있습니다. 불편하시더라도 가능한 대중교통
+              이용을 권장 드립니다.
             </span>
           </div>
-          <div className="flex gap-0.5">
+          <div className={styles.descContentSub}>
             *
             <span>
               만차 시 <b>국립현대미술관, 경복궁</b> 주차장 이용 부탁 드립니다.
