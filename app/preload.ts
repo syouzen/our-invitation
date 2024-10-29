4b595c025a1091418b394719c20ee7a3dcfda432
syouzen <seungyeon.choi1026@gmail.com>
2024-10-30 00:31:40 +0900

    🌱 only video preload

--- app/preload.ts
+++ app/preload.ts
@@ -7,29 +7,29 @@ export function PreloadResources() {
     'https://kjhswhyddvck27143991.gcdn.ntruss.com/our-invitation.mp4',
     {as: 'video'},
   );
-  ReactDOM.preload(
-    'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_0421.JPG',
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1506.JPG',
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1593.JPG',
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1730.JPG',
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1986.JPG',
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_2436.JPG',
-    {as: 'image'},
-  );
+  // ReactDOM.preload(
+  //   'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_0421.JPG',
+  //   {as: 'image'},
+  // );
+  // ReactDOM.preload(
+  //   'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1506.JPG',
+  //   {as: 'image'},
+  // );
+  // ReactDOM.preload(
+  //   'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1593.JPG',
+  //   {as: 'image'},
+  // );
+  // ReactDOM.preload(
+  //   'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1730.JPG',
+  //   {as: 'image'},
+  // );
+  // ReactDOM.preload(
+  //   'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_1986.JPG',
+  //   {as: 'image'},
+  // );
+  // ReactDOM.preload(
+  //   'https://kjhswhyddvck27143991.gcdn.ntruss.com/IMG_2436.JPG',
+  //   {as: 'image'},
+  // );
   return null;
 }
