2d2b6d8111df436df7a9566c929104dbc0cb4b8d
syouzen <seungyeon.choi1026@gmail.com>
2025-02-14 11:16:20 +0900

    🎨 calendar

--- app/components/Calendar/index.tsx
+++ app/components/Calendar/index.tsx
@@ -17,12 +17,10 @@ const Day = ({day, className}: {day: number; className?: string}) => {
 const Calendar = () => {
   return (
     <div className="flex flex-col gap-[12px] text-[14px] leading-[20px]">
-      <div className="flex flex-row justify-center items-center gap-[16px]">
-        <div className="text-gray-900 text-[16px] leading-[24px] mb-[16px]">
-          11월
-        </div>
+      <div className="flex flex-row justify-center items-center gap-[16px] text-gray-900 text-[16px] leading-[24px] mb-[12px]">
+        11월
       </div>
-      <div className="flex flex-row justify-center items-center gap-[16px]">
+      <div className="grid grid-cols-7 gap-[12px]">
         <Day day={26} className="text-transparent" />
         <Day day={27} className="text-transparent" />
         <Day day={28} className="text-transparent" />
@@ -30,8 +28,6 @@ const Calendar = () => {
         <Day day={30} className="text-transparent" />
         <Day day={31} className="text-transparent" />
         <Day day={1} />
-      </div>
-      <div className="flex flex-row justify-center items-center gap-[16px]">
         <Day day={2} />
         <Day day={3} />
         <Day day={4} />
@@ -42,8 +38,6 @@ const Calendar = () => {
           day={8}
           className="text-gray-900 border border-solid border-gray-900 rounded-full flex justify-center items-center"
         />
-      </div>
-      <div className="flex flex-row justify-center items-center gap-[16px]">
         <Day day={9} />
         <Day day={10} />
         <Day day={11} />
@@ -51,8 +45,6 @@ const Calendar = () => {
         <Day day={13} />
         <Day day={14} />
         <Day day={15} />
-      </div>
-      <div className="flex flex-row justify-center items-center gap-[16px]">
         <Day day={16} />
         <Day day={17} />
         <Day day={18} />
@@ -60,8 +52,6 @@ const Calendar = () => {
         <Day day={20} />
         <Day day={21} />
         <Day day={22} />
-      </div>
-      <div className="flex flex-row justify-center items-center gap-[16px]">
         <Day day={23} />
         <Day day={24} />
         <Day day={25} />
@@ -69,8 +59,6 @@ const Calendar = () => {
         <Day day={27} />
         <Day day={28} />
         <Day day={29} />
-      </div>
-      <div className="flex flex-row justify-center items-center gap-[16px]">
         <Day day={30} />
         <Day day={1} className="text-transparent" />
         <Day day={2} className="text-transparent" />
