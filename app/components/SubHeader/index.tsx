bcc6d155891a02b242322b0deb5a148485ca3ff7
syouzen <seungyeon.choi1026@gmail.com>
2025-03-03 04:03:50 +0900

    🌱 subheader

--- app/components/SubHeader/index.tsx
+++ app/components/SubHeader/index.tsx
@@ -8,27 +8,6 @@ const Header = () => {
   return (
     <>
       <Intersection>
-        <div className="text-center text-[14px] leading-[20px] text-gray-900">
-          봄의 그대는 벚꽃이었고
-          <br />
-          여름의 그대는 바람이었으며
-          <br />
-          가을의 그대는 하늘이었고
-          <br />
-          겨울의 그대는 하얀 눈이었다
-          <br />
-          그대는 언제나 행복, 그 자체였다.
-          <br />
-          <br />
-          {'강현욱 < 사계 > 중에서'}
-        </div>
-      </Intersection>
-
-      <div className="flex justify-center items-center my-[48px] text-gray-300">
-        <span>-</span>
-      </div>
-
-      <Intersection>
         <div className="text-center text-[14px] leading-[20px] text-gray-900 mb-[64px]">
           저의 두 걸음은
           <br />
@@ -52,6 +31,10 @@ const Header = () => {
           축복해 주시면 더 없는 기쁨이 되겠습니다.
         </div>
       </Intersection>
+
+      <div className="flex justify-center items-center my-[48px] text-gray-300">
+        <span>-</span>
+      </div>
 
       <Intersection>
         <div className="text-[14px] leading-[20px] text-gray-900 flex mt-[12px] justify-center items-center gap-[12px]">
