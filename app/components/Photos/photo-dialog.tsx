e74c7b1c2f433be6488fd91505023a01a440f753
syouzen <seungyeon.choi1026@gmail.com>
2025-07-09 23:28:24 +0900

    🎨 update z-index values in PhotoDialog component for improved overlay and content stacking

--- app/components/Photos/photo-dialog.tsx
+++ app/components/Photos/photo-dialog.tsx
@@ -31,14 +31,14 @@ const PhotoDialog = ({open, images, index, onOpenChange}: PhotoDialogProps) => {
     <DialogPrimitive.Root open={open} onOpenChange={onOpenChange}>
       <DialogPrimitive.Overlay
         className={cn(
-          'fixed inset-0 z-51 bg-white/80 backdrop-blur-sm',
+          'fixed inset-0 z-[999] bg-white/80 backdrop-blur-sm',
           'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
         )}
         onClick={() => onOpenChange(false)}
       />
       <DialogPrimitive.Content
         className={cn(
-          'container w-full z-50 sm:max-w-[600px]',
+          'container w-full z-[999] sm:max-w-[600px]',
           'fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]',
           'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
         )}
