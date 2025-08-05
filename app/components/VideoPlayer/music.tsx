8f4a2657ec3ada482a9a154a3beb3554e8712cd6
syouzen <seungyeon.choi1026@gmail.com>
2025-08-05 21:54:42 +0900

    🎵 change music

--- app/components/VideoPlayer/music.tsx
+++ app/components/VideoPlayer/music.tsx
@@ -21,7 +21,7 @@ const Music = () => {
           webkit-playsinline="webkit-playsinline"
         >
           <source
-            src="https://kr.object.ncloudstorage.com/zen-bucket/this_happy_madness.mp3"
+            src="https://kr.object.ncloudstorage.com/zen-bucket/just_the_two_of_us.mp3"
             type="audio/mp3"
           />
         </video>
