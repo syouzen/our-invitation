5db47e001936ade862238c10d14f12ed17d87ced
syouzen <seungyeon.choi1026@gmail.com>
2025-08-20 20:39:39 +0900

    🎨 preload images

--- app/preload.ts
+++ app/preload.ts
@@ -3,57 +3,59 @@
 import ReactDOM from 'react-dom';
 
 export function PreloadResources() {
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/our_video.mp4`,
-    {as: 'video'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/our_music.mp3`,
-    {as: 'audio'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/dugahun_1.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/dugahun_4.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/dugahun_6.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_1.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_2.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_3.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_4.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_5.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_6.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/og.webp`,
-    {as: 'image'},
-  );
-  ReactDOM.preload(
-    `${process.env.NEXT_PUBLIC_IMAGE_SERVER}/kakao_og.webp`,
-    {as: 'image'},
-  );
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/our_video.mp4`, {
+    as: 'video',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/our_music.mp3`, {
+    as: 'audio',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/dugahun_1.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/dugahun_4.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/dugahun_6.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_1.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_2.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_3.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_4.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_5.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/last_6.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/add_1.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/add_2.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/add_3.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/add_4.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/add_5.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/og.webp`, {
+    as: 'image',
+  });
+  ReactDOM.preload(`${process.env.NEXT_PUBLIC_IMAGE_SERVER}/kakao_og.webp`, {
+    as: 'image',
+  });
   return null;
 }
